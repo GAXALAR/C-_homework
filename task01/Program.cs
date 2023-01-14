7ca9ba603d31e99cf6cf087fa558a2a59332f32c
GAXALAR <ay.kosyrev@gmail.com>
2023-01-14 18:02:18 +0300

    дз по первому семинару С#

---
+++ task01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+
+if ( x > y)
+Console.WriteLine($"большее число= {x} меньшее число= {y}");
+
+else
+Console.WriteLine($"большее число= {y} меньшее число= {x}");  
